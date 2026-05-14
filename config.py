cda553b759bf2c51ad1ce8a2283d4835b76a1ea5
Ak Mohd Arief bin Mustapha <akarief@mampunet.btm>
2026-05-14 12:51:17 +0800

    fix vercel config

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:1234@localhost:5432/Helpdesk'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-helpdesk-key'
-    
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-helpdesk-key')
+
     # Email Settings
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'your-email@gmail.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'your-app-password'
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME') or 'your-email@gmail.com'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME')
